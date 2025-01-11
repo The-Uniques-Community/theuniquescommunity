a310ca506e0603cfebd5e60e715fdd9b96f2cde1
SuperiorSU <sujalsinha2001@gmail.com>
2025-01-11 16:03:16 +0530

    l

--- src/utils/Buttons/Button.jsx
+++ src/utils/Buttons/Button.jsx
@@ -1,11 +1,17 @@
 import { useTheme } from '@mui/material/styles';
 import { Link } from 'react-router';
-const Button = ({children}, props) => {
+import { GoArrowUpRight } from "react-icons/go";
+
+const Button = ({children, path,bgColor,color,iconColor}) => {
   const theme = useTheme();
   return (
-    <Link to={props.path}>
-      <button className=''>
+    <Link to={path}>
+      <button style={{backgroundColor: bgColor,color: color}} className={`relative px-5 py-2.5 overflow-hidden group bg-[${bgColor}] hover:bg-gradient-to-r hover:from-[${bgColor}] hover:to-[${bgColor}] text-white hover:ring-2 hover:ring-offset-2 hover:ring-[${bgColor}] transition-all ease-out duration-300`}>
+      <span class="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-20 rotate-12 group-hover:-translate-x-40 ease"></span>
         {children}
+        <span className=''>
+          <GoArrowUpRight className='ms-2 inline-block group-hover:rotate-45 duration-75' size={20} color={iconColor} />
+        </span>
       </button>
     </Link>
 
---
+++ src/utils/Card/MemberCard.jsx
@@ -0,0 +1,12 @@
+import { useTheme } from '@mui/material/styles';
+
+const MemberCard = () => {
+    const theme = useTheme();
+  return (
+    <div className=''>
+
+    </div>
+  )
+}
+
+export default MemberCard
