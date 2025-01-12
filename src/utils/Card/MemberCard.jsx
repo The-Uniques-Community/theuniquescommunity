1327eb5b339cf9ab139bede92759d5c8afc84721
SuperiorSU <sujalsinha2001@gmail.com>
2025-01-12 23:36:19 +0530

    l

--- src/utils/Card/MemberCard.jsx
+++ src/utils/Card/MemberCard.jsx
@@ -8,14 +8,14 @@ const MemberCard = ({ linkedin, instagram, twitter, fullName, batch, position, p
   const theme = useTheme();
 
   return (
-    <div className={`bg-white hover:shadow-lg duration-75 px-2 py-3 border-t-[1px] shadow-md max-w-64 relative`}>
+    <div className={`bg-white group hover:cursor-pointer hover:shadow-lg duration-75 px-2 py-3 border-t-[1px] shadow-md max-w-64 relative`}>
       {/* Profile Image */}
       <div className='overflow-hidden rounded-lg'>
         <img src={profileImg} className="hover:scale-105 duration-150  custom-clip rounded-t-lg rounded-l-lg" alt={`${fullName}'s Profile`} />
       </div>
       
       {/* Social Media Links */}
-      <div className="flex flex-col gap-y-4 absolute right-2 top-0 bottom-0 items-center justify-start py-5 bg-black w-12">
+      <div className="flex flex-col gap-y-4 absolute right-2 top-0 bottom-36 items-center justify-start py-5 bg-black w-12">
         {/* Twitter */}
         {twitter && (
           <Link to={twitter} target="_blank">
@@ -53,7 +53,7 @@ const MemberCard = ({ linkedin, instagram, twitter, fullName, batch, position, p
       {/* Name and Position */}
       <div className="font-bold text-xl">
         <p className="border-b-[1px] border-slate-500">{fullName}</p>
-        <span className="font-light text-slate-700 text-sm -mt-3">/{position}</span>
+        <span className="font-light text-slate-700 text-sm -mt-3">//{position}</span>
       </div>
 
       {/* Batch */}
@@ -62,9 +62,9 @@ const MemberCard = ({ linkedin, instagram, twitter, fullName, batch, position, p
       </div>
 
       {/* Arrow Link */}
-      <div className="absolute right-16 bottom-4">
+      <div className="absolute right-7 bottom-4">
         
-          <div onClick={onClick} className="w-12 h-12 rounded-full group flex bg-black justify-center items-center">
+          <div onClick={onClick} className="w-12 h-12 rounded-full flex bg-black justify-center items-center">
             <GoArrowUpRight className="inline-block group-hover:rotate-45 duration-75" size={20} color="white" />
           </div>
         
