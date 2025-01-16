e107a17428e437e8e1020c13b21a813664c9b05b
SuperiorSU <sujalsinha2001@gmail.com>
2025-01-16 11:25:58 +0530

    backend setup

--- src/utils/Container/ContainerFull.jsx
+++ src/utils/Container/ContainerFull.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const ContainerFull = ({children,bgColor}) => {
   return (
-    <div style={{backgroundColor: bgColor}} className='p-4 m-3 rounded-md lg:min-h-screen md:min-h-screen min-h-screen w-full mx-auto'>
+    <div style={{backgroundColor: bgColor}} className='p-4 m-3 rounded-md w-full mx-auto'>
         {children}
     </div>
   )
 }
 
-export default ContainerFull
+export default ContainerFull;
