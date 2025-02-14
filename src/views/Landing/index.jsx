054a37637863df3c810079ee62e22126fa93d78f
SuperiorSU <sujalsinha2001@gmail.com>
2025-02-14 12:28:15 +0530

    blog data added

--- src/views/Landing/index.jsx
+++ src/views/Landing/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useTheme } from "@mui/material/styles";
 import Footer from "@/utils/Footer/Footer";
-import Navbar from "@/utils/NavBar/Navbar";
+import BlogCard from "@/utils/Card/BlogCard";
+import { blogData } from "@/assets/dummyData/blogData";
 import { reviews } from "@/assets/dummyData/reviewData";
 import tu from "@/assets/logos/theuniquesCommunity.png";
 import CallToAction from "./homComponents/CallToAction";
@@ -237,195 +238,16 @@ const index = () => {
   dark:[&::-webkit-scrollbar-track]:bg-neutral-200
   dark:[&::-webkit-scrollbar-thumb]:bg-neutral-400 snap-x"
                 >
-                  {/* <div className="relative snap-start scroll-ml-6 shrink-0 first:pl-6 last:pr-6">
-                    <div className="relative flex flex-col overflow-hidden transition-all duration-200 transform bg-white border border-gray-100 shadow w-60 md:w-80 group rounded-xl hover:shadow-lg hover:-translate-y-1">
-                      <a
-                        href="#"
-                        title=""
-                        className="flex shrink-0 aspect-w-4 aspect-h-3"
-                      >
-                        <img
-                          className="object-cover w-full h-full transition-all duration-200 transform group-hover:scale-110"
-                          src="https://landingfoliocom.imgix.net/store/collection/clarity-blog/images/hero/4/thumbnail-1.png"
-                          alt="thumbnail-1"
-                        />
-                      </a>
-                      <div className="flex-1 px-4 py-5 sm:p-6">
-                        <a href="#" title="" className="">
-                          <p className="text-lg font-bold text-gray-900">
-                            How to write content about your photographs
-                          </p>
-                          <p className="mt-3 text-sm font-normal leading-6 text-gray-500 line-clamp-3">
-                            Lorem ipsum dolor sit amet, consec tetur adipiscing
-                            elit. Sit quis auctor odio arcu et dolor.
-                          </p>
-                        </a>
-                      </div>
-                      <div className="px-4 py-5 mt-auto border-t border-gray-100 sm:px-6">
-                        <div className="flex items-center justify-between">
-                          <div className="flex items-center space-x-2">
-                            <p className="text-sm font-medium text-gray-900">
-                              <a href="#" title="" className="">
-                                Growth
-                              </a>
-                            </p>
-                            <span className="text-sm font-medium text-gray-900">
-                              •
-                            </span>
-                            <p className="text-sm font-medium text-gray-900">
-                              7 Mins Read
-                            </p>
-                          </div>
-                          <a href="#" title="" className="" role="button">
-                            <svg
-                              className="w-5 h-5 text-gray-300 transition-all duration-200 group-hover:text-gray-900"
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewbox="0 0 24 24"
-                              stroke-width="2"
-                              stroke="currentColor"
-                              fill="none"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            >
-                              <path
-                                stroke="none"
-                                d="M0 0h24v24H0z"
-                                fill="none"
-                              ></path>
-                              <line x1="17" y1="7" x2="7" y2="17"></line>
-                              <polyline points="8 7 17 7 17 16"></polyline>
-                            </svg>
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                  </div> */}
-                  <div className="relative snap-start scroll-ml-6 shrink-0 first:pl-6 last:pr-6">
-                    <div className="relative flex flex-col overflow-hidden transition-all duration-200 transform bg-white border border-gray-100 shadow w-60 md:w-80 group rounded-xl hover:shadow-lg hover:-translate-y-1">
-                      <a
-                        href="#"
-                        title=""
-                        className="flex shrink-0 aspect-w-4 aspect-h-3"
-                      >
-                        <img
-                          className="object-cover w-full h-full transition-all duration-200 transform group-hover:scale-110"
-                          src="https://landingfoliocom.imgix.net/store/collection/clarity-blog/images/hero/4/thumbnail-2.png"
-                          alt="thumbnail-2"
-                        />
-                      </a>
-                      <div className="flex-1 px-4 py-5 sm:p-6">
-                        <a href="#" title="" className="">
-                          <p className="text-lg font-bold text-gray-900">
-                            How to write content about your photographs
-                          </p>
-                          <p className="mt-3 text-sm font-normal leading-6 text-gray-500 line-clamp-3">
-                            Lorem ipsum dolor sit amet, consec tetur adipiscing
-                            elit. Sit quis auctor odio arcu et dolor.
-                          </p>
-                        </a>
-                      </div>
-                      <div className="px-4 py-5 mt-auto border-t border-gray-100 sm:px-6">
-                        <div className="flex items-center justify-between">
-                          <div className="flex items-center space-x-2">
-                            <p className="text-sm font-medium text-gray-900">
-                              <a href="#" title="" className="">
-                                Growth
-                              </a>
-                            </p>
-                            <span className="text-sm font-medium text-gray-900">
-                              •
-                            </span>
-                            <p className="text-sm font-medium text-gray-900">
-                              7 Mins Read
-                            </p>
-                          </div>
-                          <a href="#" title="" className="" role="button">
-                            <svg
-                              className="w-5 h-5 text-gray-300 transition-all duration-200 group-hover:text-gray-900"
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewbox="0 0 24 24"
-                              stroke-width="2"
-                              stroke="currentColor"
-                              fill="none"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            >
-                              <path
-                                stroke="none"
-                                d="M0 0h24v24H0z"
-                                fill="none"
-                              ></path>
-                              <line x1="17" y1="7" x2="7" y2="17"></line>
-                              <polyline points="8 7 17 7 17 16"></polyline>
-                            </svg>
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="relative snap-start scroll-ml-6 shrink-0 first:pl-6 last:pr-6">
-                    <div className="relative flex flex-col overflow-hidden transition-all duration-200 transform bg-white border border-gray-100 shadow w-60 md:w-80 group rounded-xl hover:shadow-lg hover:-translate-y-1">
-                      <a
-                        href="#"
-                        title=""
-                        className="flex shrink-0 aspect-w-4 aspect-h-3"
-                      >
-                        <img
-                          className="object-cover w-full h-full transition-all duration-200 transform group-hover:scale-110"
-                          src="https://landingfoliocom.imgix.net/store/collection/clarity-blog/images/hero/4/thumbnail-3.png"
-                          alt="thumbnail-3"
-                        />
-                      </a>
-                      <div className="flex-1 px-4 py-5 sm:p-6">
-                        <a href="#" title="" className="">
-                          <p className="text-lg font-bold text-gray-900">
-                            How to write content about your photographs
-                          </p>
-                          <p className="mt-3 text-sm font-normal leading-6 text-gray-500 line-clamp-3">
-                            Lorem ipsum dolor sit amet, consec tetur adipiscing
-                            elit. Sit quis auctor odio arcu et dolor.
-                          </p>
-                        </a>
-                      </div>
-                      <div className="px-4 py-5 mt-auto border-t border-gray-100 sm:px-6">
-                        <div className="flex items-center justify-between">
-                          <div className="flex items-center space-x-2">
-                            <p className="text-sm font-medium text-gray-900">
-                              <a href="#" title="" className="">
-                                Growth
-                              </a>
-                            </p>
-                            <span className="text-sm font-medium text-gray-900">
-                              •
-                            </span>
-                            <p className="text-sm font-medium text-gray-900">
-                              7 Mins Read
-                            </p>
-                          </div>
-                          <a href="#" title="" className="" role="button">
-                            <svg
-                              className="w-5 h-5 text-gray-300 transition-all duration-200 group-hover:text-gray-900"
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewbox="0 0 24 24"
-                              stroke-width="2"
-                              stroke="currentColor"
-                              fill="none"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            >
-                              <path
-                                stroke="none"
-                                d="M0 0h24v24H0z"
-                                fill="none"
-                              ></path>
-                              <line x1="17" y1="7" x2="7" y2="17"></line>
-                              <polyline points="8 7 17 7 17 16"></polyline>
-                            </svg>
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  {blogData.slice(0, 3).map((blog, index) => (
+                    <BlogCard
+                      key={index}
+                      title={blog.title}
+                      description={blog.description}
+                      category={blog.category}
+                      readTime={blog.readTime}
+                      image={blog.image}
+                    />
+                  ))}
                 </div>
               </div>
             </div>
@@ -438,7 +260,7 @@ const index = () => {
       </section>
       <div className="spacer py-10"></div>
       <section>
-        <CorporateCultureSection/>
+        <CorporateCultureSection />
       </section>
       <div className="spacer py-10"></div>
       <section>
@@ -456,7 +278,8 @@ const index = () => {
           </h4>
           <h2 className="text-3xl text-center lg:text-4xl font-bold mb-8">
             A new way to manage your
-            <br />online money.
+            <br />
+            online money.
           </h2>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-6 lg:px-16 px-6 py-4 lg:py-8">
