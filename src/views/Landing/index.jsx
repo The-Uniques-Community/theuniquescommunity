88530de44f4e74101049ef93ac69344e2a9f5a80
SuperiorSU <sujalsinha2001@gmail.com>
2025-02-14 12:11:53 +0530

    layout for landing done

--- src/views/Landing/index.jsx
+++ src/views/Landing/index.jsx
@@ -9,44 +9,44 @@ import LandingStats from "./homComponents/LandingStats";
 import AboutSection from "./homComponents/AboutSection";
 import TrainingTabs from "./homComponents/TrainingTabs";
 import ReviewCard from "@/utils/Card/ReviewCard";
-import ShapedCard from "@/utils/Card/ShapedCard";
+// import ShapedCard from "@/utils/Card/ShapedCard";
 import CorporateCultureSection from "./homComponents/CorporateCultureSection";
 const index = () => {
   const theme = useTheme();
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <section>
-        <div class="">
-          <section class="relative lg:h-screen bg-gray-50">
-            <div class="px-4 py-12 sm:py-16 sm:px-6 lg:px-0 lg:max-w-7xl lg:mx-auto lg:py-24 xl:py-36 lg:grid lg:grid-cols-2">
-              <div class="lg:pr-8">
-                <div class="max-w-md mx-auto sm:max-w-lg lg:mx-0">
+        <div className="">
+          <section className="relative lg:h-screen bg-gray-50">
+            <div className="px-4 py-12 sm:py-16 sm:px-6 lg:px-0 lg:max-w-7xl lg:mx-auto lg:py-24 xl:py-36 lg:grid lg:grid-cols-2">
+              <div className="lg:pr-8">
+                <div className="max-w-md mx-auto sm:max-w-lg lg:mx-0">
                   <img src={tu} className="mb-5 w-full object-center" alt="" />
-                  <h1 class="text-xl font-medium text-gray-900 sm:text-xl lg:text-xl">
+                  <h1 className="text-xl font-medium text-gray-900 sm:text-xl lg:text-xl">
                     Community of designers{" "}
-                    <span class="inline">
+                    <span className="inline">
                       <img
-                        class="inline w-auto h-5 sm:h-7 lg:h-8"
+                        className="inline w-auto h-5 sm:h-7 lg:h-8"
                         src="https://landingfoliocom.imgix.net/store/collection/clarity-blog/images/hero/4/shape-1.svg"
                         alt="shape-1"
                       />
                     </span>{" "}
                     made by designers
-                    <span class="inline">
+                    <span className="inline">
                       <img
-                        class="inline w-auto h-5 sm:h-7 lg:h-8"
+                        className="inline w-auto h-5 sm:h-7 lg:h-8"
                         src="https://landingfoliocom.imgix.net/store/collection/clarity-blog/images/hero/4/shape-2.svg"
                         alt="shape-2"
                       />
                     </span>
                   </h1>
-                  <p class="mt-6 text-base font-normal leading-7 text-gray-900">
+                  <p className="mt-6 text-base font-normal leading-7 text-gray-900">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Vehicula massa in enim luctus. Rutrum arcu.
                   </p>
                   <svg
-                    class="w-auto h-4 mt-8 text-gray-300"
+                    className="w-auto h-4 mt-8 text-gray-300"
                     viewbox="0 0 172 16"
                     fill="none"
                     stroke="currentColor"
@@ -197,14 +197,14 @@ const index = () => {
                       transform="matrix(-0.5547 0.83205 0.83205 0.5547 144 1)"
                     ></line>
                   </svg>
-                  <p class="mt-8 text-base font-bold text-gray-900">
+                  <p className="mt-8 text-base font-bold text-gray-900">
                     Join to get free updates every week
                   </p>
-                  <form action="#" method="post" class="relative mt-4">
-                    <div class="absolute transitiona-all duration-1000 opacity-30 inset-0 bg-gradient-to-r from-black via-[#FF44EC] to-[#ca0019] rounded-xl blur-lg filter group-hover:opacity-100 group-hover:duration-200"></div>
-                    <div class="relative space-y-4 sm:flex sm:space-y-0 sm:items-end">
-                      <div class="flex-1">
-                        <label for="" class="sr-only">
+                  <form action="#" method="post" className="relative mt-4">
+                    <div className="absolute transitiona-all duration-1000 opacity-30 inset-0 bg-gradient-to-r from-black via-[#FF44EC] to-[#ca0019] rounded-xl blur-lg filter group-hover:opacity-100 group-hover:duration-200"></div>
+                    <div className="relative space-y-4 sm:flex sm:space-y-0 sm:items-end">
+                      <div className="flex-1">
+                        <label for="" className="sr-only">
                           Email address
                         </label>
                         <div>
@@ -212,14 +212,14 @@ const index = () => {
                             type="email"
                             name=""
                             id=""
-                            class="block w-full px-4 py-3 sm:py-3.5 text-base font-medium text-gray-900 placeholder-gray-500 border border-gray-300 rounded-lg sm:rounded-l-lg sm:rounded-r-none sm:text-sm focus:ring-gray-900 focus:border-gray-900"
+                            className="block w-full px-4 py-3 sm:py-3.5 text-base font-medium text-gray-900 placeholder-gray-500 border border-gray-300 rounded-lg sm:rounded-l-lg sm:rounded-r-none sm:text-sm focus:ring-gray-900 focus:border-gray-900"
                             placeholder="Enter email address"
                           />
                         </div>
                       </div>
                       <button
                         type="button"
-                        class="inline-flex items-center justify-center w-full sm:w-auto px-8 py-3 sm:text-sm text-base sm:py-3.5 font-semibold text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-lg sm:rounded-r-lg sm:rounded-l-none hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                        className="inline-flex items-center justify-center w-full sm:w-auto px-8 py-3 sm:text-sm text-base sm:py-3.5 font-semibold text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-lg sm:rounded-r-lg sm:rounded-l-none hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                       >
                         Join Now
                       </button>
@@ -228,7 +228,7 @@ const index = () => {
                 </div>
               </div>
             </div>
-            <div class="">
+            <div className="">
               <div className="flex flex-col items-center justify-center overflow-hidden lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
                 <div
                   className="flex justify-start w-full gap-6 pb-8 overflow-x-auto [&::-webkit-scrollbar]:h-1
@@ -237,7 +237,7 @@ const index = () => {
   dark:[&::-webkit-scrollbar-track]:bg-neutral-200
   dark:[&::-webkit-scrollbar-thumb]:bg-neutral-400 snap-x"
                 >
-                  <div className="relative snap-start scroll-ml-6 shrink-0 first:pl-6 last:pr-6">
+                  {/* <div className="relative snap-start scroll-ml-6 shrink-0 first:pl-6 last:pr-6">
                     <div className="relative flex flex-col overflow-hidden transition-all duration-200 transform bg-white border border-gray-100 shadow w-60 md:w-80 group rounded-xl hover:shadow-lg hover:-translate-y-1">
                       <a
                         href="#"
@@ -299,7 +299,7 @@ const index = () => {
                         </div>
                       </div>
                     </div>
-                  </div>
+                  </div> */}
                   <div className="relative snap-start scroll-ml-6 shrink-0 first:pl-6 last:pr-6">
                     <div className="relative flex flex-col overflow-hidden transition-all duration-200 transform bg-white border border-gray-100 shadow w-60 md:w-80 group rounded-xl hover:shadow-lg hover:-translate-y-1">
                       <a
