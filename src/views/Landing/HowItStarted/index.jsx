8051bb847a5f7c897e70f8e4ea39653c9bb3f01e
Abhishek <141233909+itsmemrityunjay@users.noreply.github.com>
2025-02-27 10:11:03 +0530

    timeline

--- src/views/Landing/HowItStarted/index.jsx
+++ src/views/Landing/HowItStarted/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from "@/views/Landing/HowItStarted/components/Header"
 import TimeLine from "@/views/Landing/HowItStarted/components/TimeLine"
 
 const index = () => {
     return (
         <div>
+            <Header />
             <TimeLine />
         </div>
     )
