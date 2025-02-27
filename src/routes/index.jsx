0a424bbd3b111e347dfe6f608b3069f4e992ec90
Your Name <abhimishra.db12@gmail.com>
2025-02-27 13:49:09 +0530

    last abhi

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -10,6 +10,8 @@ import Auth from "@/routes/Authentication/Auth";
 import About from "@/views/Landing/About/index"
 import HowItStarted from "@/views/Landing/HowItStarted/index"
 
+import Event from "@/views/Landing/Event/Index"
+
 
 const Landing = Loader(lazy(() => import("@/views/Landing/index")));
 // const MemberLogin = Loader(lazy(() => import("@/views/Landing/MemberLogin/index")));
@@ -25,11 +27,11 @@ const LandingRoutes = {
     { index: true, element: <Landing /> }, // Default route ("/")
     { path: "about", element: <About /> },
     { path: "howitstarted", element: <div><HowItStarted /></div> },
-    { path: "events", element: <div>Events</div> },
+    { path: "events", element: <Event/> },
     { path: "community-page", element: <div>Community Page</div> },
     { path: "training", element: <div>Training</div> },
     // { path: "login", element: <div>Login</div> },
-    { path: "batches", element: <div>Batches</div> },
+    { path: "batches", element: <></>},
     // { path: "member-login", element: <MemberLogin /> },      all login will be in login router with diffrent layout please find the folder in routes
     // { path: "member-register", element: <MemberRegister /> },
     { path: "blogs", element: <BlogPage /> },
