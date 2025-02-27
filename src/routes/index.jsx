48106ae8b5826de7b94ce69d76a2c8698bd961b4
Nemmen <aryankammboz@gmail.com>
2025-02-27 10:13:28 +0530

    wwe

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,12 +6,15 @@ import CommunityRoutes from "@/routes/Community/CommunityRoutes";
 import CoordinatorRoutes from "@/routes/Uniques/CoordinatorRoute";
 import MemberRoutes from "@/routes/Uniques/MemberRoutes";
 import LandingLayout from "@/layout/Landing/index";
+import Auth from "@/routes/Authentication/Auth";
 import About from "@/views/Landing/About/index"
 
 
 const Landing = Loader(lazy(() => import("@/views/Landing/index")));
-const MemberLogin = Loader(lazy(() => import("@/views/Landing/MemberLogin/index")));
-const MemberRegister = Loader(lazy(() => import("@/views/Landing/MemberRegister/index")));
+// const MemberLogin = Loader(lazy(() => import("@/views/Landing/MemberLogin/index")));
+// const MemberRegister = Loader(lazy(() => import("@/views/Landing/MemberRegister/index")));
+
+
 const LandingRoutes = {
   path: "/",
   element: <LandingLayout />, // Wrap all pages inside LandingLayout
@@ -23,19 +26,21 @@ const LandingRoutes = {
     { path: "blog", element: <div>Blog</div> },
     { path: "community-page", element: <div>Community Page</div> },
     { path: "training", element: <div>Training</div> },
-    { path: "login", element: <div>Login</div> },
+    // { path: "login", element: <div>Login</div> },
     { path: "batches", element: <div>Batches</div> },
-    {path: "member-login", element: <MemberLogin/>},
-    {path: "member-register", element: <MemberRegister/>},
+    // {path: "member-login", element: <MemberLogin/>},      all login will be in login router with diffrent layout please find the folder in routes
+    // {path: "member-register", element: <MemberRegister/>},
   ],
 };
 
 const router = createBrowserRouter([
+  
   LandingRoutes,  // ✅ Fix: Use object directly, no spread operator
   AdminRoutes,
   CommunityRoutes,
   CoordinatorRoutes,
   MemberRoutes,
+  Auth
 ]);
 
 export default router;
