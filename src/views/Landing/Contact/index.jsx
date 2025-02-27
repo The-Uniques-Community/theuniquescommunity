3693403767d5c6cdf902ed6f50fc0cdcf088592a
SuperiorSU <sujalsinha2001@gmail.com>
2025-02-27 13:58:50 +0530

    pulled from main

--- src/views/Landing/Contact/index.jsx
+++ src/views/Landing/Contact/index.jsx
@@ -4,7 +4,7 @@ import ContainerFull from '@/utils/Container/ContainerFull'
 
 const index = () => {
   return (
-    <ContainerFull bgImg="https://img.freepik.com/premium-photo/cup-coffee-notebook-earphone-tulips-black-wooden-table-flat-lay-copy-space_649760-201.jpg">
+    <ContainerFull >
         <ContactForm/>
     </ContainerFull>
     
