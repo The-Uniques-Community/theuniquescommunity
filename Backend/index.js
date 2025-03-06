979353e8717f3637898ba354566052517aeba484
Nemmen <aryankammboz@gmail.com>
2025-03-07 03:23:13 +0530

    dvs

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,8 @@ import cookieParser from "cookie-parser";
 import memberRoute from "./routes/member/memberRoute.js";
 import adminRoute from "./routes/admin/adminRoute.js";
 import blogRouter from "./routes/member/blogRoute.js";
+import passport from './config/passport.js';
+import authRoutes from './routes/Authentication/auth.routes.js';
 dotenv.config();
 
 const app = express();
@@ -17,6 +19,7 @@ app.use(
     credentials: true,
   })
 );
+app.use(passport.initialize());
 
 app.get("/", (req, res) => {
   res.send("hello its working");
@@ -25,6 +28,7 @@ app.get("/", (req, res) => {
 app.use("/api/member", memberRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/blog", blogRouter);
+app.use("/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
