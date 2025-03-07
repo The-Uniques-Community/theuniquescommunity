b0a7b2fbaff8f66a95374d5980791ca7c173dcf2
Abhishek <141233909+itsmemrityunjay@users.noreply.github.com>
2025-03-07 13:03:42 +0530

    Final Community

--- src/views/Community/index.jsx
+++ src/views/Community/index.jsx
@@ -3,6 +3,8 @@ import Hero from './Landing/Hero'
 import SlideCard from './Landing/SlideCard'
 import About from './Landing/About'
 import Guidelines from './Landing/Guidlines'
+import CTA from "./Landing/CallToAction"
+import Eligiblity from './Landing/Eligiblity'
 
 const index = () => {
 	return (
@@ -10,7 +12,9 @@ const index = () => {
 			<Hero />
 			<About />
 			<Guidelines />
+			<CTA />
 			<SlideCard />
+			{/* <Eligiblity /> */}
 			<div className='p-10'></div>
 		</div>
 	)
