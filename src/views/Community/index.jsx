3c03bfc7605fd521043254c39fec24f4d082b57a
Your Name <abhimishra.db12@gmail.com>
2025-03-24 11:29:11 +0530

    jio

--- src/views/Community/index.jsx
+++ src/views/Community/index.jsx
@@ -3,10 +3,10 @@ import Hero from './Landing/Hero'
 import SlideCard from './Landing/SlideCard'
 import About from './Landing/About'
 import Guidelines from './Landing/Guidlines'
-import CTA from "./Landing/CallToAction"
+
 import Eligiblity from './Landing/Eligiblity'
 import CTA from "./Landing/CallToAction"
-import Eligiblity from './Landing/Eligiblity'
+
 import Sliding from "./Landing/Sliding"
 
 const index = () => {
