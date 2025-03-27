3e673460e338c6da1fac85e176a77bf12d5f0933
SuperiorSU <sujalsinha2001@gmail.com>
2025-03-27 09:32:12 +0530

    lol2

--- Backend/routes/landing/publicMemberRouter.js
+++ Backend/routes/landing/publicMemberRouter.js
@@ -9,7 +9,6 @@ const publicMemberRouter = express.Router();
 
 // Get all public members with optional filtering
 publicMemberRouter.get("/", getPublicMembers);
-
 // Get a single public member by ID
 
 publicMemberRouter.get("/counts", getMemberCounts)
