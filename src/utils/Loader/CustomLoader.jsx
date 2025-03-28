ac89500d7814bb226eb7a721581be9d447184a6a
Nemmen <aryankammboz@gmail.com>
2025-03-28 16:09:34 +0530

    AS

--- src/utils/Loader/CustomLoader.jsx
+++ src/utils/Loader/CustomLoader.jsx
@@ -1,10 +1,41 @@
-import LinearProgress from "@mui/material/LinearProgress";
-import Box from "@mui/material/Box";
+import React from 'react';
 
-const CustomLoader = () => (
-  <Box sx={{ position: "fixed", top: 0, left: 0, zIndex: 1301, width: "100%" }}>
-    <LinearProgress color="primary" />
-  </Box>
-);
+const CustomLoader = ({ size = 'medium', color = '#ca0019' }) => {
+  // Size variants
+  const sizes = {
+    small: 24,
+    medium: 40,
+    large: 60
+  };
+  
+  const actualSize = sizes[size] || sizes.medium;
+  
+  return (
+    <div 
+      className="custom-loader"
+      style={{
+        width: actualSize,
+        height: actualSize,
+        borderRadius: '50%',
+        display: 'inline-block',
+        borderTop: `2px solid ${color}`,
+        borderRight: '2px solid transparent',
+        boxSizing: 'border-box',
+        animation: 'rotation 1s linear infinite'
+      }}
+    >
+      <style jsx>{`
+        @keyframes rotation {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export default CustomLoader;
