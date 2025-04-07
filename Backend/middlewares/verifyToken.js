0fd1d3c11200586df5dc44742e6aab74c7f01aaf
SuperiorSU <sujalsinha2001@gmail.com>
2025-04-07 14:22:37 +0530

    all set for blog

--- Backend/middlewares/verifyToken.js
+++ Backend/middlewares/verifyToken.js
@@ -7,7 +7,8 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const verifyToken = (req, res, next) => {
   // Get the token from cookies
   const token = req.cookies.token; // Make sure 'cookie-parser' middleware is used
-
+  console.log("Token from cookies:", token);
+  console.log(req.cookies)
   
   if (!token) {
     return res.status(401).json({ error: 'No token provided' });
--- Backend/routes/member/blogRoute.js
+++ Backend/routes/member/blogRoute.js
@@ -1,12 +1,24 @@
-import express from "express";
-import { createBlog, getAllBlogs, getBlogById } from "../../controller/member/blogController.js";
-
+import express from 'express';
+import { 
+  createBlog, 
+  getAllBlogs, 
+  getMyBlogs,
+  getBlogsByMember, 
+  editBlog, 
+  deleteBlogById 
+} from '../../controller/member/blogController.js';
+import verifyToken from '../../middlewares/verifyToken.js';
+ 
 const blogRouter = express.Router();
 
-blogRouter.post("/", createBlog);          // Create a new blog
-blogRouter.get("/", getAllBlogs);          // Get all blogs
-blogRouter.get("/:id", getBlogById);       // Get a single blog by ID
+// Public routes - no authentication needed
+blogRouter.get('/', getAllBlogs);
+blogRouter.get('/member/:memberId', getBlogsByMember);
 
+// Protected routes - require authentication
+blogRouter.post('/', verifyToken, createBlog);
+blogRouter.get('/my-blogs', verifyToken, getMyBlogs);
+blogRouter.put('/:blogId', verifyToken, editBlog);
+blogRouter.delete('/:blogId', verifyToken, deleteBlogById);
 
 export default blogRouter;
-
