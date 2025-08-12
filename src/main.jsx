7058b3ccfc0b0e1853fbae10384f995cc6a16e97
Nemmen <aryankammboz@gmail.com>
2025-08-12 08:02:52 +0530

    asd

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,24 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import 'lenis/dist/lenis.css'
 import { ThemeProvider } from "./theme/ThemeProvider.jsx";
 import { ToastContainer } from "react-toastify";
 import store from "@/redux/store";
 import { Provider } from "react-redux";
 import ClickSpark from "./utils/clickSpark/ClickSpark";
+import Lenis from 'lenis'
+
+
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+// lenis.on('scroll', (e) => {
+//   // console.log(e);
+// });
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
