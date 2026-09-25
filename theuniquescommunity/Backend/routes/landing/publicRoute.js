db48815243d944bce1171b800e2b6f88278885bd
AshishSharma-014 <ashishkrsh0014@gmail.com>
2026-09-25 19:24:20 +0530

    Revert "nidhi"
    This reverts commit 38bdbea32d78c36ffa0366c210335e44e73d30b7.

--- theuniquescommunity/Backend/routes/landing/publicRoute.js
+++
@@ -1,8 +0,0 @@
-import { submitContactForm } from "../../controller/admin/enquiryController.js";
-import express from "express";
-
-const publicEnquiryRouter = express.Router();
-
-publicEnquiryRouter.post("/", submitContactForm);
-
-export default publicEnquiryRouter;
--- theuniquescommunity/src/views/Uniques/Coordinator/Community/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const index = () => {
-  return (
-    <div>Community</div>
-  )
-}
-
-export default index
